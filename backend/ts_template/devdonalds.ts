994335f0f280d8faf39de462e0fe400db1d3466d
Brigette <z5587785@ad.unsw.edu.au>
2026-02-27 22:14:49 +1100

    copied over relevant parts of 2025 app, refactoring part 3 approach to actually do the spec rip

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 
 // ==== Type Definitions, feel free to add or modify ==========================
-interface cookbookEntry {
+abstract class cookbookEntry {
   name: string;
-  type: string;
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
 interface requiredItem {
@@ -11,12 +13,22 @@ interface requiredItem {
   quantity: number;
 }
 
-interface recipe extends cookbookEntry {
+class recipe extends cookbookEntry {
   requiredItems: requiredItem[];
+
+  constructor(name: string, requiredItems: requiredItem[]) {
+    super(name);
+    this.requiredItems = requiredItems;
+  }
 }
 
-interface ingredient extends cookbookEntry {
+class ingredient extends cookbookEntry {
   cookTime: number;
+
+  constructor(name: string, cookTime: number) {
+    super(name);
+    this.cookTime = cookTime;
+  }
 }
 
 // =============================================================================
@@ -26,7 +38,10 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+// maps a name to an existing entry (if add_entry was used)
+const cookbook = new Map<string, cookbookEntry>();
+// lazy lookup thing for part 3
+const ingredients = new Map<string, ingredient>();
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -45,25 +60,107 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  const dash_to_wspace = /-|_/g;
+  const only_alphabetical = /[^a-zA-Z ]/g;
+  // also collapses whitespace
+  const remove_dashes = (recipeName.replace(dash_to_wspace, ' ')).replace(/ +/g, ' ');
+  const remove_non_alpha = remove_dashes.replace(only_alphabetical, '').toLowerCase();
+
+  if (remove_non_alpha.length === 0) {
+    return null;
+  }
+  const capitalised_list = remove_non_alpha.split(' ').map(x => x[0].toUpperCase() + x.slice(1));
+
+  const res = capitalised_list.join(' ');
+  return res;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  const { type, name } = req.body;
+  let extra;
+  // typeguarding due to dual type nature 
+  if ('requiredItems' in req.body) {
+    extra = req.body.requiredItems;
+  } else if ('cookTime' in req.body) {
+    extra = req.body.cookTime;
+  }
 
+  try {
+    const result = add_entry(type, name, extra);
+    res.send(200);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
+
+const add_entry = (type: string, name: string, extra: number|requiredItem[]) => {
+  // errors: if the name already exists, if cooktime < 0, type is not recipe/ingredient
+  if (type !== 'recipe' && type !== 'ingredient') {
+    throw Error('Type must be recipe/ingredient');
+  } else if (cookbook.has(name)) {
+    throw Error('Name already exists');
+  }
+
+  if (typeof extra === 'number') {
+    if (extra < 0) {
+      throw Error('Cook time must be a non negative number');
+    } else {
+      const newEntry = new ingredient(name, extra);
+      cookbook.set(name, newEntry);
+      ingredients.set(name, newEntry);
+    }
+  } else {
+    // Recipe requiredItems can only have one element per name.
+    const copy_required_items = new Set(extra.slice().map(x => x.name));
+    // diff lengths means theres dupes
+    if (copy_required_items.size !== extra.length) {
+      throw Error('Recipe requiredItems can only have one element per name');
+    } else {
+      cookbook.set(name, new recipe(name, extra));
+    }
+  }
+
+  return { };
+};
+
+
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
-
+  const name = req.query.name;
+  try {
+    const result = summarise(name);
+    res.json(result);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
+
+const summarise = (name: string) => {
+  // algo: take recipe and recursively determine base ingredients + decompose nested recipes
+
+  if (!cookbook.has(name)) {
+    throw Error('The item' + name + 'cannot be found.');
+  } 
+
+  const baseRecipe = cookbook[name];
+  if (baseRecipe instanceof ingredient) {
+    throw Error(name + 'is an ingredient.');
+  }
+
+
+
+
+
+  /**
+   * A recipe with the corresponding name cannot be found.
+   * The searched name is NOT a recipe name (ie. an ingredient).
+   * The recipe contains recipes or ingredients that aren't in the cookbook.
+   */
+};
 
 // =============================================================================
 // ==== DO NOT TOUCH ===========================================================
